6aad2462c92eaa2c09c7d45ed32b888290fa3a47
d-twin <dmytro.blyznyuk@gmail.com>
2016-04-20 18:16:31 +0300

    add codersterra app welcome screen

--- codersterra/js/my-app.js
+++ codersterra/js/my-app.js
@@ -1,5 +1,6 @@
 // Initialize your app
 var myApp = new Framework7({swipePanel:'left'});
+
 // Export selectors engine
 var $$ = Dom7;
 
@@ -14,3 +15,30 @@ var view4 = myApp.addView('#view-4');
 
 var mainView = myApp.addView('.view-main');
 
+var welcomescreen_slides = [
+    {
+        id: 'slide0',
+        picture: '<div class="tutorialicon">♥</div>',
+        text: 'Welcome to this tutorial. In the next steps we will guide you through a manual that will teach you how to use this app.'
+    },
+    {
+        id: 'slide1',
+        picture: '<div class="tutorialicon">✲</div>',
+        text: 'This is slide 2'
+    },
+    {
+        id: 'slide2',
+        picture: '<div class="tutorialicon">♫</div>',
+        text: 'This is slide 3'
+    },
+    {
+        id: 'slide3',
+        picture: '<div class="tutorialicon">☆</div>',
+        text: 'Thanks for reading! Enjoy this app.<br><br><a class="tutorial-close-btn" href="#">End Tutorial</a>'
+    }
+];
+var options = {
+    'bgcolor': '#0da6ec',
+    'fontcolor': '#fff'
+};
+var welcomescreen = myApp.welcomescreen(welcomescreen_slides, options);
